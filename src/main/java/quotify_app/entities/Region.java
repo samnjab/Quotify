ef8ab0784210847688afaf3b64050ac4cf4217cb
samjaberi <samanjaberi.sj@gmail.com>
2024-11-16 19:45:50 -0500

    modified the structure of the region interface to match the region blueprint.

--- src/main/java/quotify_app/entities/Region.java
+++ src/main/java/quotify_app/entities/Region.java
@@ -17,4 +17,40 @@ public interface Region {
      */
     String getRegionId();
 
+    /**
+     * Returns the type of the region.
+     * @return the type of the region.
+     */
+    String getType();
+
+    /**
+     * Returns the code of the region composed of letters.
+     * @return the code of the region.
+     */
+    String getCode();
+
+    /**
+     * Returns the country the region resides in .
+     * @return the Country object of the region.
+     */
+    Country getCountry();
+
+    /**
+     * Returns the state the region resides in .
+     * @return the State object of the region.
+     */
+    State getState();
+
+    /**
+     * Returns the city the region resides in .
+     * @return the City object of the region.
+     */
+    City getCity();
+
+    /**
+     * Returns the county the region resides in.
+     * @return the County object of the region.
+     */
+    County getCounty();
+
 }
