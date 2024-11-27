0996ddfa39fde9b4a840e8f8ca56bdd71c1bfa19
samjaberi <samanjaberi.sj@gmail.com>
2024-11-27 00:54:18 -0500

    - first draft LandingViewModel - first draft LandingState

--- src/main/java/quotify_app/adapters/landing/LandingState.java
+++ src/main/java/quotify_app/adapters/landing/LandingState.java
@@ -1,34 +1,85 @@
 package quotify_app.adapters.landing;
 
+import quotify_app.entities.regionEntities.Area;
+
 /**
  * The state for the Landing View Model.
  */
 public class LandingState {
-    private String username = "";
-    private String password = "";
-    private String loginError = "";
 
-    public String getUsername() {
-        return username;
+    private Area selectedCountry;
+    private Area selectedState;
+    private Area selectedCity;
+    private String streetAddress;
+    private boolean isLoggedIn;
+    private String errorMessage;
+
+    public LandingState() {
+        this.selectedCountry = null;
+        this.selectedState = null;
+        this.selectedCity = null;
+        this.streetAddress = "";
+        this.isLoggedIn = false;
+        this.errorMessage = "";
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public Area getSelectedCountry() {
+        return selectedCountry;
     }
 
-    public String getPassword() {
-        return password;
+    public void setSelectedCountry(Area selectedCountry) {
+        this.selectedCountry = selectedCountry;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public Area getSelectedState() {
+        return selectedState;
     }
 
-    public String getLoginError() {
-        return loginError;
+    public void setSelectedState(Area selectedState) {
+        this.selectedState = selectedState;
     }
 
-    public void setLoginError(String loginError) {
-        this.loginError = loginError;
+    public Area getSelectedCity() {
+        return selectedCity;
+    }
+
+    public void setSelectedCity(Area selectedCity) {
+        this.selectedCity = selectedCity;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * Resets the state of the landing page to its default.
+     */
+    public void resetState() {
+        this.selectedCountry = null;
+        this.selectedState = null;
+        this.selectedCity = null;
+        this.streetAddress = "";
+        this.isLoggedIn = false;
+        this.errorMessage = "";
     }
 }
