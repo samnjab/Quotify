c932fbfe84334b22b815e8cee0fc45eab15d4c06
samjaberi <samanjaberi.sj@gmail.com>
2024-11-28 21:43:49 -0500

    - Overloaded Property constructor for API exception handling.

--- src/main/java/quotify_app/adapters/landing/LandingController.java
+++ src/main/java/quotify_app/adapters/landing/LandingController.java
@@ -1,13 +1,14 @@
 package quotify_app.adapters.landing;
 
+import java.util.List;
+
 import quotify_app.entities.regionEntities.Area;
-import quotify_app.usecases.landing.LandingInputBoundary;
 import quotify_app.usecases.landing.AddressInputData;
+import quotify_app.usecases.landing.LandingInputBoundary;
 
 /**
  * The controller for actions performed on the landing page.
  */
-
 public class LandingController {
 
     private final LandingInputBoundary landingInteractor;
@@ -17,28 +18,41 @@ public class LandingController {
     }
 
     /**
-     * Executes the Select Address usecase.
-     * @param country the name of the country to pass to the interactor.
-     * @param state the name of the state to pass to the interactor.
-     * @param city the name of the city to pass to the interactor.
-     * @param zipCode the zipCode to pass to the interactor.
-     * @param stAddress the street address of the property to pass to the interactor.
-     * @throws Exception when the selected region is not found within the database.
+     * Executes the Select Address use case.
+     * @param country the selected country.
+     * @param state   the selected state.
+     * @param city    the selected city.
+     * @param zipCode the selected zip code area.
+     * @param address the street address of the property.
+     * @throws Exception when address selection fails.
      */
-    public void selectRegion(Area country, Area state, Area city, String stAddress) throws Exception {
-        final AddressInputData addressInputData = new AddressInputData(country, state, city, stAddress);
+    public void selectAddress(Area country, Area state, Area city, Area zipCode, String address) {
+        final AddressInputData addressInputData = new AddressInputData(country, state, city, zipCode, address);
         landingInteractor.selectAddress(addressInputData);
     }
 
     /**
-     * Triggers the navigation to SignupView through the LandingInteractor.
+     * Fetches available countries from the interactor.
+     * @return a list of available countries.
+     * @throws RuntimeException when interactor fails at fetching countries.
+     */
+    public void fetchCountries() {
+        landingInteractor.fetchCountries();
+    }
+
+    public List<String> fetchAddressSuggestions(String partialAddress) throws Exception {
+        return landingInteractor.autoCompleteAddress(partialAddress);
+    }
+
+    /**
+     * Triggers navigation to the signup view.
      */
     public void goToSignup() {
         landingInteractor.goToSignup();
     }
 
     /**
-     * Triggers the navigation to LoginView through the LandingInteractor.
+     * Triggers navigation to the login view.
      */
     public void goToLogin() {
         landingInteractor.goToLogin();
